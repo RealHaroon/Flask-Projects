8370ad7ff198af8e4374f28b07c59e72d038e2df
Haroon Khan <133528235+RealHaroon@users.noreply.github.com>
2025-07-13 22:56:19 +0500

    Creating Notes app in Flask ( 13-7-2025 )

--- Notes-App/Components/routes.py
+++ Notes-App/Components/routes.py
@@ -1,6 +1,46 @@
 from flask import render_template,request,redirect,url_for,flash
 from Components import app
+from Components.models import Note,db
 
 @app.route('/')
 def index():
-    return 'This is Index page of notes app'
+    notes = Note.query.order_by(Note.updated_at.desc()).all()
+    return render_template('index.html', notes=notes)
+
+
+@app.route('/',methods=['POST'])
+def  add_note():
+    title=request.form['title']
+    content=request.form['content']
+
+
+    if not title or not content:
+        flash('Title and Content cannot be empty!',category='warning')
+        return  redirect(url_for('index'))
+
+
+    newNote=Note(title=title,content=content)
+    db.session.add(Note)
+    db.session.commit()
+    flash('Note Added Successfully!',category='success')
+    return redirect(url_for('index'))
+
+
+@app.route('/edit/<int:id>',methods=['GET','POST'])
+def edit_note(id):
+    note=Note.query.get_or_404(id)
+    if  request.method  =='POST':
+        note.title=request.form['title']
+        note.content=request.form['content']
+        db.session.commit()
+        flash('Note  updated  successfully!',category='success')
+        return redirect(url_for('index'))
+    
+
+@app.route('/delete/<int:id>')
+def delete_note(id):
+    note  = Note.query.get_or_404(id)
+    db.session.delete(note)
+    db.session.commit()
+    flash('Note Deleted Successfully!',category='success')
+    return redirect(url_for('index'))
